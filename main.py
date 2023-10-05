8b7535bc5808369679999a4b0d224590dcd409f1
junkie63 <bss443901.il@gmail.com>
2023-10-05 21:45:55 +0300

    Part2

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-cook_book = {
-    'Омлет': [{'ingedient_name': 'Яйцо', 'quantity': '2', 'measure': 'шт'},
-              {'ingedient_name': 'Молоко', 'quantity': '100', 'measure': 'мл'},
-              {'ingedient_name': 'Помидор', 'quantity': '2', 'measure': 'шт'}
-              ],
-    'Утка по-пекински': [{'ingedient_name': 'Утка', 'quantity': '1', 'measure': 'шт'},'\n',
-                         {'ingedient_name': 'Вода', 'quantity': '2', 'measure': 'л'},
-                         {'ingedient_name': 'Мед', 'quantity': '3', 'measure': 'ст. л'},
-                         {'ingedient_name': 'Соевый соус', 'quantity': '60', 'measure': 'мл'}
-                         ],
-    'Запеченный картофель': [{'ingedient_name': 'Картофель', 'quantity': '1', 'measure': 'кг'},
-                             {'ingedient_name': 'Чеснок', 'quantity': '3', 'measure': 'зубч.'},
-                             {'ingedient_name': 'Сыр гауда', 'quantity': '100', 'measure': 'гр.'}
-                             ]
-}
-with open('text.txt', 'w') as file:
-    for dish, ingrs in cook_book.items():
-        for ingr in ingrs:
-            for ing in ingr.values():
-                file.write(f'Название блюда: {dish} \nКоличество ингредиентов в блюде: \n{ing}')
+#def my_cook_book():
+from pprint import  pprint
+with open("text.txt", encoding="utf-8") as file:
+    cook_book = {}
+    for line in file.read().split(("\n\n")):
+        #print(line)
+        dish_name, *ingredients = line.split("\n")
+        cook_list = []
+        #print(ingredients)
+        for ingredient in ingredients[1:]:
+            ingredient_name, quantity, measure = ingredient.split(" | ")
+            cook_list.append(
+                {
+                    "ingredient_name": ingredient_name,
+                    "quantity": int(quantity),
+                    "measure": measure,
+                }
+            )
+        cook_book[dish_name] = cook_list
+    del cook_book["Фахитос"]
+    #print(cook_list)
+pprint(f"cook_book = {cook_book}")
 
 
-
+def get_shop_list_by_dishes(dish_name, person_count):
+    for dish in cook_book:
+        for ingredients in cook_book[dish]:
+            for ingredient, measure in ingredients.items():
+                print(measure)
+#     for dish in cook_book:
+print(get_shop_list_by_dishes("Омлет",5))
+#
+#         else:
+#             print("Такого блюда нет в меню")
+#     print(my_cook_book)
+#
+# print(get_shop_list_by_dishes("Омлет", 7))
+# print(new_cook)
