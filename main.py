bcefa5e78a4f39d40347e7f5813631c5480dea20
junkie63 <bss443901.il@gmail.com>
2023-10-08 19:42:24 +0300

    Final

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-#def my_cook_book():
-from pprint import  pprint
-with open("text.txt", encoding="utf-8") as file:
-    cook_book = {}
-    for line in file.read().split(("\n\n")):
-        #print(line)
-        dish_name, *ingredients = line.split("\n")
-        cook_list = []
-        #print(ingredients)
-        for ingredient in ingredients[1:]:
-            ingredient_name, quantity, measure = ingredient.split(" | ")
-            cook_list.append(
-                {
-                    "ingredient_name": ingredient_name,
-                    "quantity": int(quantity),
-                    "measure": measure,
-                }
-            )
-        cook_book[dish_name] = cook_list
+def my_cook_book():
+    from pprint import  pprint
+    with open("text.txt", encoding="utf-8") as file:
+        cook_book = {}
+        for line in file.read().split(("\n\n")):
+            #print(line)
+            dish_name, *ingredients = line.split("\n")
+            cook_list = []
+            #print(ingredients)
+            for ingredient in ingredients[1:]:
+                ingredient_name, quantity, measure = ingredient.split(" | ")
+                cook_list.append(
+                    {
+                        "ingredient_name": ingredient_name,
+                        "quantity": int(quantity),
+                        "measure": measure,
+                    }
+                )
+            cook_book[dish_name] = cook_list
+    return cook_book
 
-
-for dish_name, ingredients in cook_book.items():
-   print(dish_name, ingredients, sep="\n")
-
+print(my_cook_book())
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     new_cook = {}
-    for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                ingredient["quantity"] *= person_count
-                new_cook.setdefault(ingredient["ingredient_name"], ingredient)
-    meal_dict = {}
-    for value in new_cook.values():
-        name = value["ingredient_name"]
-        del value["ingredient_name"]
-        meal_dict[name] = value
-    return  meal_dict
+    for dish, ingredients in my_cook_book().items():
+        if dish in dishes:
+            for ingredient in ingredients:
+                name, quantity, measure = ingredient.values()
+                new_cook.setdefault(name, {"measure": measure, "quantity": 0})
+                new_cook[name]["quantity"] += quantity * person_count
+    return  new_cook
 
 
-print(get_shop_list_by_dishes(["Омлет"], 5))
+print(get_shop_list_by_dishes(["Омлет", "Фахитос"], 2))
 
